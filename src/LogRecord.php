fdf303b77d3ba42716e4fb649899d0d331d24acf
alfan06 <alfannoufal@gmail.com>
2025-06-12 13:43:09 +0700

    v0.2.0(1)

--- src/LogRecord.php
+++ src/LogRecord.php
@@ -1,12 +1,17 @@
 <?php
 
-namespace Coreproc\LaravelCustomLogging;
+namespace AdevPmftc\LaravelCustomLogging;
 
 class LogRecord extends \Monolog\LogRecord
 {
     public function toArray(): array
     {
         $array = parent::toArray();
+
+        // Remove datetime
+        unset($array['datetime']);
+
+        // Add origin
         return array_merge(['origin' => 'app.' . config('custom-logging.container_role')], $array);
     }
 }
